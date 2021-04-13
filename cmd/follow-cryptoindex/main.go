47460878d07d0dd0be29fd6c036993dc652dfc4b
Patryk Woziński <patryk.wozinski@gmail.com>
2021-04-13 22:23:10 +0200

    init client of go-gecko

--- cmd/follow-cryptoindex/main.go
+++ cmd/follow-cryptoindex/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	coingecko "github.com/superoo7/go-gecko/v3"
+	"net/http"
+	"time"
 )
 
 func main() {
+	httpClient := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	coingecko.NewClient(httpClient)
+
 	fmt.Printf("Buy high, sell low :)")
 }
